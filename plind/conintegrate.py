343c702d71b1a367c0abfad9a1a67173dd0fb6ee
Dylan Jow <djow@physics.utoronto.ca>
2019-08-01 11:26:52 -0400

    Filled out some functions in plmodel

---
+++ plind/conintegrate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# conintegrate.py - performs contour integral
+import numpy as np
+from scipy.interpolate import splprep, splev
+from scipy.integrate import simps
+
+def conintegrate(f, line, Nint):
+    tck, u = splprep([line.real, line.imag], s=0)  # it is not clear what s means, but s = None (default) breaks things
+    param_grid = np.linspace(u[0], u[-1], Nint)
+    vec = splev(param_grid, tck)
+    pts = vec[0]+1j*vec[1]
+    dpts = splev(param_grid, tck, der=1)
+    deriv = dpts[0]+1j*dpts[1]
+    integral = simps(f(pts)*deriv, x=param_grid)
+    return integral
--- plind/plmodel.py
+++ plind/plmodel.py
@@ -1,5 +1,7 @@
 import numpy as np
+from scipy.misc import derivative
 from .solution import solution
+from .conintegrate import conintegrate
 
 class plmodel:
     """some documentation."""
@@ -14,14 +16,25 @@ class plmodel:
         self.integral = 0
         self.critpts = []
 
+
     def get_contour(self):
         return self.contour
 
-    def get_trajector(self):
+
+    def get_trajectory(self):
         pass
+
 
     def get_expfun(self):
         return self.expfun
+
+
+    def get_intfun(self):
+        """Return integrand function, i.e. np.exp(self.expfun(z, args=self.expargs))."""
+        def intfun(z, args=self.expargs):
+            return np.exp(self.expfun(z, *args))
+        return intfun
+
 
     def get_morse(self):
         """Return the morse function, i.e. the real part of expfun."""
@@ -29,21 +42,34 @@ class plmodel:
             return self.expfun(z, *args).real
         return morse
 
-    def get_grad(self):
+
+    def get_grad(self, dx=1e-6):
         """Return self.grad. If self.grad is none, returns numerical gradient computed from self.expfun."""
         if self.grad is None:
-            pass
+            morse = self.get_morse()
+
+            def num_grad(z):
+                gradRe = -derivative(morse, x0=z, dx=dx)
+                gradIm = derivative(morse, x0=z, dx=dx)
+                return gradRe.real + 1j*gradIm.imag
+
+            return num_grad
         else:
             return self.grad
+
 
     def get_integral(self):
         return self.integral
 
+
     def get_critpts(self):
         return self.critpts
+
 
     def descend(self):
         pass
 
-    def integrate(self):
-        pass
+
+    def integrate(self, Nint=1000):
+        intfun = self.get_intfun()
+        self.integral = conintegrate(intfun, self.contour, Nint=Nint)
