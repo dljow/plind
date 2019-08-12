c4a501b2e0588caa8cddb35a8ada9e1f38e73610
Fang Xi Lin <flin@cita.utoronto.ca>
2019-08-12 17:19:15 -0400

    added cylinder projections

--- plind/projection/core.py
+++ plind/projection/core.py
@@ -62,3 +62,63 @@ def sphere_to_plane_vec(pt, vec):
     vec_z = vec_u + 1j * vec_v
     return vec_z
 
+def plane_to_cyl(z):
+    """
+    Project a point from the complex plane to a cylinder, wrapping around the real axis, embedded in R^3
+    """
+    u = z.real
+    v = z.imag
+    denom = 1+u**2
+
+    X = 2*u / denom
+    Y = v
+    Z = (u**2 + - 1) / denom
+
+    pt = np.array([X, Y, Z])
+
+    return pt
+
+def plane_to_cyl_vec(z, vec_z):
+    """
+    Pushforward a tangent vector from the complex plane to a cylinder, embedded in R^3
+    """
+    u = z.real
+    v = z.real
+
+    vec_u = vec_z.real
+    vec_v = vec_z.imag
+
+    denom = (1 + u**2)**2
+
+    vec_X = 2 * (1-u**2)/denom * vec_u
+    vec_Y = vec_v
+    vec_Z = 4*u/denom * vec_u
+
+    vec = np.array([vec_X,vec_Y,vec_Z])
+    return vec
+
+def cyl_to_plane(pt):
+    """
+    Project a point from the Riemann sphere to the complex plane.
+    """
+    X, Y, Z = pt[0], pt[1], pt[2]
+    u = X / (1-Z)
+    v = Y
+
+    z = u + 1j*v
+
+    return z
+
+def cyl_to_plane_vec(pt, vec):
+    """
+    Pushforward a tangent vector from the Riemann sphere to the complex plane.
+    """
+    X, Y, Z = pt[0], pt[1], pt[2]
+    vec_X, vec_Y, vec_Z = vec[0], vec[1], vec[2]
+
+    vec_u = vec_X/(1-Z) + vec_Z * X/(1-Z)**2
+    vec_v = vec_Y
+
+    vec_z = vec_u + 1j * vec_v
+    return vec_z
+
