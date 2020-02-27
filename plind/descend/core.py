fbc01034e9b0908266d4447dc2245d89c4896d64
Emily Tyhurst <etyhurst@physics.utoronto.ca>
2020-02-27 12:10:58 -0800

    One line fix to the dimension. Added pole identifier in descend.

--- plind/descend/core.py
+++ plind/descend/core.py
@@ -1,9 +1,17 @@
 import numpy as np
 from ..projection import *
 
+def identify_pole(points, gradh, dt, expargs, thresh=10):
+    # performs Euler time step
+    if np.any((np.abs(gradh(np.array(points), *expargs))/np.mean(np.abs(gradh(np.array(points), *expargs))))>thresh):
+              print("A pole was found")
+
 def flow(points, gradh, dt, expargs=[]):
     # performs Euler time step
+   # identify_pole(points, gradh, dt, expargs)
     return np.array(points)+gradh(np.array(points), *expargs)*dt
+
+
 
 
 # def flow_eq(time, line, gradh, args=[], anchor=[]):
