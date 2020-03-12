eee34f92842b35ad9172c1fef3acfd6ffd23191f
Fang Xi Lin <flin@cita.utoronto.ca>
2020-03-12 14:11:48 -0400

    RK4 implemented, but multiple expargs not working

--- plind/descend/core.py
+++ plind/descend/core.py
@@ -6,7 +6,20 @@ def identify_pole(points, gradh, dt, expargs, thresh=10):
     if np.any((np.abs(gradh(np.array(points), *expargs))/np.mean(np.abs(gradh(np.array(points), *expargs))))>thresh):
               print("A pole was found")
 
+def _euler(points, gradh, dt, expargs):
+    return points + dt * gradh(points, expargs)
+
+def _rk4(points, gradh, dt, expargs):
+    #
+    k1 = dt * gradh(points, expargs)
+    k2 = dt * gradh(points + k1/2, expargs)
+    k3 = dt * gradh(points + k2/2, expargs)
+    k4 = dt * gradh(points + k3, expargs)
+    return points + (k1 + 2*k2 + 2*k3 + k4)/6
+
 def flow(points, gradh, dt, expargs=[]):
     # performs Euler time step
    # identify_pole(points, gradh, dt, expargs)
-    return np.array(points)+gradh(np.array(points), *expargs)*dt
+    #flow_method = _euler
+    flow_method = _rk4
+    return flow_method(points, gradh, dt, *expargs)
