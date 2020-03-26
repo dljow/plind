e716589b58610d76e0d1efc76b0394ef56c4cba3
Dylan Jow <djow@physics.utoronto.ca>
2020-03-26 15:47:08 -0400

    broken quadpy integrator

--- plind/integrate/core.py
+++ plind/integrate/core.py
@@ -4,22 +4,32 @@ import numpy as np
 from scipy.interpolate import splprep, splev
 from scipy.integrate import simps, quadrature, fixed_quad
 from ..projection import *
+import quadpy
 
-def _vol(points):
-    """Computes the volume of the (ndim)-simplex"""
-    ndim = points.shape[-1]
-    mat = (points - points[0])[1:]
-    # the sqrt(mat@mat) ensures that the orientation of all the simplices are consistent, but the overall integral can be off by an overall minus sign
-    # TODO: fix the overall minus sign possibly by tracking orientations.
-    if (np.shape(mat) == (1,1)) or (np.shape(mat) == (1,)):
-        dV = np.sqrt(np.squeeze(mat)**2)
-    else:
-        dV = np.sqrt(np.linalg.det(mat@mat))/np.math.factorial(ndim)
-    return dV
+def conintegrate(f, contour, args=[], order=3):
+    scheme = quadpy.nsimplex.grundmann_moeller(contour.ndim, order)
+    simps = np.stack(contour.points(contour.simplices), axis=-2)
 
-def conintegrate(f, contour, args=[]):
-    integral = 0j
-    for simp in contour.simplices:
-        mid = np.sum(contour.points[simp], 0)/len(simp)
-        integral += f(mid, *args)*_vol(contour.points[simp])
-    return integral
+    def f_quadpy(x):
+        pass
+    val = sheme.integrate(f_quadpy, simps)
+    return sum(val)
+    
+# def _vol(points):
+#     """Computes the volume of the (ndim)-simplex"""
+#     ndim = points.shape[-1]
+#     mat = (points - points[0])[1:]
+#     # the sqrt(mat@mat) ensures that the orientation of all the simplices are consistent, but the overall integral can be off by an overall minus sign
+#     # TODO: fix the overall minus sign possibly by tracking orientations.
+#     if (np.shape(mat) == (1,1)) or (np.shape(mat) == (1,)):
+#         dV = np.sqrt(np.squeeze(mat)**2)
+#     else:
+#         dV = np.sqrt(np.linalg.det(mat@mat))/np.math.factorial(ndim)
+#     return dV
+#
+# def conintegrate(f, contour, args=[]):
+#     integral = 0j
+#     for simp in contour.simplices:
+#         mid = np.sum(contour.points[simp], 0)/len(simp)
+#         integral += f(mid, *args)*_vol(contour.points[simp])
+#     return integral
