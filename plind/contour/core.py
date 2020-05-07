9e378906f5b1fda6e02843b25cc649b14563f430
Emily Tyhurst <etyhurst@physics.utoronto.ca>
2020-05-07 12:22:18 -0700

    Added testfunction class in preparation for testing.

--- plind/contour/core.py
+++ plind/contour/core.py
@@ -135,6 +135,7 @@ class contour:
                          a certain threshold. 
            
         """
+        
         used_simps = np.array([], dtype=np.int)
         uni_bad_edges = np.array([], dtype=np.int)
         uni_bad_simps = np.array([], dtype=np.int)
---
+++ plind_tests/gaussian.py
@@ -0,0 +1,50 @@
+from .testfunction import testfunction
+import numpy as np
+
+
+def Gauss_exp(z, ndim):
+    gauss_sum=0
+    for i in range(ndim):
+        gauss_sum= gauss_sum + z[i]**2
+    return 1j*(gauss_sum)
+
+def Gauss_gradh(z, ndim):
+    return 2*1j*np.conj(z)
+
+def Gauss_answer(ndim):
+    return (np.pi*1j)**(ndim/2)
+
+def Gauss_thimble(x, ndim):
+    return x+1j*x
+
+def Gauss_thimble_dist(point, ndim):
+    return np.abs(point.real - point.imag)
+
+def Gaussian(ndim):
+    """Returns the Gaussian integral of n-dimensions as a test function.
+       
+       Parameters
+       ----------
+        ndim: integer
+            The dimension of the Gaussian.
+            
+       Returns
+       ----------
+        GaussianTestFn: plind.tests.testfunction Object
+            A test function structure for testing plind.py
+
+       See Also
+       --------
+           
+        testfunction: A Test Function with known Lefshetz Thimbles, gradient, and integral over R^n.    
+    """
+    expfun = Gauss_exp
+    gradh = Gauss_gradh
+    expargs = [ndim]
+    ndim = ndim
+    thimbles = Gauss_thimble
+    thimble_dist = Gauss_thimble_dist
+    integral = Gauss_answer
+        
+    return testfunction(expfun, gradh, expargs, ndim, thimbles, thimble_dist, integral)
+    
