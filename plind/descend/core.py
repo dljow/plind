b79ec67ca1461a4413893e339a327d5c02af7d52
Dylan Jow <djow@physics.utoronto.ca>
2020-12-11 15:48:58 -0500

    Cleaned up descend/core.py. Fixed linter errors.

--- plind/descend/core.py
+++ plind/descend/core.py
@@ -1,10 +1,5 @@
 import numpy as np
 
-def identify_pole(points, gradh, dt, expargs, thresh=10):
-    # performs Euler time step
-    if np.any((np.abs(gradh(np.array(points), *expargs))/np.mean(np.abs(gradh(np.array(points), *expargs))))>thresh):
-              print("A pole was found")
-
 def _euler(points, gradh, dt, expargs):
     return points + dt * gradh(points, expargs), dt
 
@@ -19,24 +14,24 @@ def _rk4(points, gradh, dt, expargs):
 def _rkf45(points, gradh, dt, expargs):
     # Runge-Kutta-Fehlberg 4(5) order integrator with adaptive time step.
 
-    #HARDCODED STEP-WISE TOLERANCE!!
+    # HARDCODED STEP-WISE TOLERANCE!!
     tol = 1e-5
 
-    k1 = dt * gradh(  points, *expargs)
-    k2 = dt * gradh(  points + k1 * 1/4, *expargs)
-    k3 = dt * gradh(  points + k1 * 3/32      + k2 * 9/32, *expargs)
-    k4 = dt * gradh(  points + k1 * 1932/2197 + k2 * -7200/2197 + k3 * 7296/2197, *expargs)
-    k5 = dt * gradh(  points + k1 * 439/216   + k2 * -8         + k3 * 3680/513   + k4 * -845/4104, *expargs)
-    k6 = dt * gradh(  points + k1 * -8/27     + k2 * 2          + k3 * -3544/2565 + k4 * 1859/4104   + k5 * -11/40, *expargs)
+    k1 = dt * gradh(points, *expargs)
+    k2 = dt * gradh(points + k1 * 1/4, *expargs)
+    k3 = dt * gradh(points + k1 * 3/32 + k2 * 9/32, *expargs)
+    k4 = dt * gradh(points + k1 * 1932/2197 + k2 * -7200/2197 + k3 * 7296/2197, *expargs)
+    k5 = dt * gradh(points + k1 * 439/216 + k2 * -8 + k3 * 3680/513 + k4 * -845/4104, *expargs)
+    k6 = dt * gradh(points + k1 * -8/27 + k2 * 2 + k3 * -3544/2565 + k4 * 1859/4104 + k5 * -11/40, *expargs)
 
-    points_next     = points + k1 * 25/216    +                   k3 * 1408/2565  + k4 * 2197/4104   + k5 * -1/5
-    points_next_alt = points + k1 * 16/135    +                   k3 * 6656/12825 + k4 * 28561/56430 + k5 * -9/50 + k6 * 2/55
+    points_next = points + k1 * 25/216 + k3 * 1408/2565 + k4 * 2197/4104 + k5 * -1/5
+    points_next_alt = points + k1 * 16/135 + k3 * 6656/12825 + k4 * 28561/56430 + k5 * -9/50 + k6 * 2/55
 
     # compute the difference between the 4th and 5th order results
     R = np.abs(points_next - points_next_alt) / dt
 
     # the next timestep is bound by the largest R, hence smallest delta
-    delta = np.min((np.divide(tol,2*R))**(1/4))
+    delta = np.min((np.divide(tol, 2*R))**(1/4))
 
     # if any difference is largest than tolerance, redo the current step with a smaller dt
     if np.any(R > tol):
@@ -47,7 +42,7 @@ def _rkf45(points, gradh, dt, expargs):
     return points_next, delta*dt
 
 def _dop853(points, gradh, dt, expargs):
-    #This would be the Dormand-Prince method as recommended by Numerical Recipes, but is not implemented at the moment. Do not use!
+    # This would be the Dormand-Prince method as recommended by Numerical Recipes, but is not implemented at the moment. Do not use!
     k1 = dt * gradh(points, *expargs)
     k2 = dt * gradh(points + k1/5, *expargs)
     k3 = dt * gradh(points + k1*3/40 + k2*9/40, *expargs)
@@ -60,8 +55,6 @@ def _dop853(points, gradh, dt, expargs):
 
 def flow(points, gradh, dt, expargs=[]):
     # performs time step
-   # identify_pole(points, gradh, dt, expargs)
-
 
     # choose a method by hand here. Eventually, we should add this as an option.
     # flow_method = _euler
