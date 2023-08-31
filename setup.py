4aef80d66cccb4d5489bdaaf9e8c722285a5240f
Fang Xi Lin <flin@cita.utoronto.ca>
2023-08-31 15:31:34 -0400

    change setup.py to pyproject.toml for modernization

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="plind",
-    version="0.1.0",
-    author="Dylan Jow, Fang Xi Lin, Emily Tyhurst",
-    description="Picard-Lefschetz integrator for highly oscillatory functions",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(where='src'),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    py_modules=["plind"],
-    package_dir={'':'src'},
-    install_requires=['numpy','scipy','matplotlib','sympy']
-)
+setuptools.setup()
